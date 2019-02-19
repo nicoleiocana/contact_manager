69e5fe565c450b932a56083a36b0daa697105676
Nicolei Ocana <nicoleiocana@gmail.com>
2019-02-19 18:53:29 +0000

    generate phone number and associate with person

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_19_174238) do
+ActiveRecord::Schema.define(version: 2019_02_19_185136) do
 
   create_table "people", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "phone_numbers", force: :cascade do |t|
+    t.string "number"
+    t.integer "person_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
