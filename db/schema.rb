c892185d2e8be00571d2972bb316f698133300b3
Nicolei Ocana <nicoleiocana@gmail.com>
2019-02-20 23:04:01 +0000

    implement scaffold generator for Company model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_20_042055) do
+ActiveRecord::Schema.define(version: 2019_02_20_230240) do
+
+  create_table "companies", force: :cascade do |t|
+    t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "email_addresses", force: :cascade do |t|
     t.string "address"
