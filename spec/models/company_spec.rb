54bcbb4231baeecbc395ca88b9f0e3412e2e67df
Nicolei Ocana <nicoleiocana@gmail.com>
2019-02-20 23:10:57 +0000

    verify valid/invalid rspec tests pass

--- spec/models/company_spec.rb
+++ spec/models/company_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Company, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:company) do
+    Company.new(name: "Startup")
+  end
+  
+  it 'is valid' do
+    expect(company).to be_valid
+  end
+  
+  it 'is invalid' do
+    company.name = nil
+    expect(company).not_to be_valid
+  end
+  
 end
